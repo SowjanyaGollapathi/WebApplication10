9c2dd4097b5eaae00b3b4fd0295dff821e987af4
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-06 12:11:19 +0530

    Add project files.

---
+++ WebApplication10/Controllers/DepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Department> departments = new List<Department>();
+            departments.Add(new Department() { DeptId = 1, DeptName = "Civil", DeptLocation = "Nellore" });
+            departments.Add(new Department() { DeptId = 2, DeptName = "EEE", DeptLocation = "Nellore" });
+            departments.Add(new Department() { DeptId = 3, DeptName = "MECH", DeptLocation = "Nellore" });
+            departments.Add(new Department() { DeptId = 4, DeptName = "ECE", DeptLocation = "Nellore" });
+            departments.Add(new Department() { DeptId = 5, DeptName = "CSE", DeptLocation = "Nellore" });
+
+            ViewData["departments"] = departments;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication10/Filters/LogAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+        private void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = string.Format("{0}- controller:{1} action:{2}", methodName, controllerName, actionName);
+
+            Debug.writeLine(message);
+
+        }
+    }
+    
+}
